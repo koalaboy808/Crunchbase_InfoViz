e1e3f3c2139a83c03256bfb28c89ec8cda463e74
gracenbrilz <gracenbrilmyer@ischool.berkeley.edu>
2016-05-05 18:54:55 -0700

    small edits, added desc.

--- UnicornsApp/public_html/js/materialize.js
+++ UnicornsApp/public_html/js/materialize.js
@@ -2481,7 +2481,7 @@ $(document).ready(function(){
 
 //          offset - 200 allows elements near bottom of page to scroll
 			
-	    	$('html, body').animate({ scrollTop: offset - 200 }, {duration: 400, queue: false, easing: 'easeOutCubic'});
+	    	$('html, body').animate({ scrollTop: offset - 20 }, {duration: 400, queue: false, easing: 'easeOutCubic'});
 			
 		  });
 		});
